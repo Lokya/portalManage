bc1bbb23b8d9b2c7474266bb272c6b2c763ea0ee
lokya <hlikan@163.com>
2019-11-05 17:15:29 +0800

    update: 删除lint

--- src/components/Menu.vue
+++ src/components/Menu.vue
@@ -1,6 +1,7 @@
 <template>
   <el-menu :default-active="activeIndex" class="el-menu-demo" mode="horizontal"
-           @select="handleSelect" :style="menuStyle">
+           @select="handleSelect" :style="menuStyle" :text-color='textColor'
+           :active-text-color='activeTextColor'>
     <el-menu-item index="1">处理中心</el-menu-item>
     <el-submenu index="2">
       <template slot="title">我的工作台</template>
@@ -21,9 +22,28 @@ export default {
         position: 'fixed',
         width: '100%',
         zIndex: 10,
-      }
-    }
-  }
+      },
+      textColor: '#fff',
+      activeTextColor: '#eee',
+    };
+  },
 };
 </script>
-
+<style lang="less">
+  .el-menu {
+    background-color: unset
+  }
+  .el-menu--horizontal>.el-menu-item:not(.is-disabled):focus,
+  .el-menu--horizontal>.el-menu-item:not(.is-disabled):hover,
+  .el-menu--horizontal>.el-submenu .el-submenu__title:hover {
+    background-color: rgba(0, 0, 0, 0.4);
+  }
+  .el-menu--horizontal .el-menu .el-menu-item,
+  .el-menu--horizontal .el-menu .el-submenu__title {
+    background-color: rgba(0, 0, 0, 0.4);
+  }
+  .el-menu--horizontal .el-menu-item:not(.is-disabled):focus,
+  .el-menu--horizontal .el-menu-item:not(.is-disabled):hover {
+    background-color: rgba(0, 0, 0, 0.7);
+  }
+</style>
