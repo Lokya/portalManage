ff98cdf745fe23254ba58ee61fd8270c0394c5d6
lokya <hlikan@163.com>
2019-11-12 17:20:05 +0800

    add: 增加滑动组件

--- src/main.js
+++ src/main.js
@@ -3,9 +3,12 @@ import ElementUI from 'element-ui';
 import 'element-ui/lib/theme-chalk/index.css';
 import './index.css';
 import App from './App.vue';
+import VueAwesomeSwiper from 'vue-awesome-swiper';
+import 'swiper/dist/css/swiper.css'
 
 Vue.config.productionTip = false;
 Vue.use(ElementUI);
+Vue.use(VueAwesomeSwiper)
 
 new Vue({
   render: h => h(App),
